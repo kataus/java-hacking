d1ecb8032e74cc452ced885296edac52f47512ab
kataus <kataus@gmail.com>
2020-10-13 23:19:24 +0300

    Первый коммит. Заготовка к занятию по SQL-Injection

---
+++ sql-injection/src/main/java/ru/itvitality/education/jh/sqlinj/Starter.java
@@ -0,0 +1,11 @@
+package ru.itvitality.education.jh.sqlinj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter {
+    public static void main(String[] __){
+        SpringApplication.run( Starter.class, __ );
+    }
+}
